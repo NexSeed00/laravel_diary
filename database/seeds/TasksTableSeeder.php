3e46049d122e603e31d7d54f6f4103d3ec9f93e5
Atsushi <atsushi88885@gmail.com>
2019-02-26 23:58:29 +0800

    db seeding

--- database/seeds/TasksTableSeeder.php
+++ database/seeds/TasksTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TasksTableSeeder extends Seeder
 {
@@ -11,6 +13,14 @@ class TasksTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        foreach (range(1, 3) as $num) {
+            DB::table('tasks')->insert([
+                'title' => 'サンプル_'.$num,
+                'status' => $num,
+                'due_date' => Carbon::now()->addDay($num),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
     }
 }
