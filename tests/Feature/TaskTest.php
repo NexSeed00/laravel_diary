de7956f128b4a89a4539ce949e537501a4f8050c
Atsushi <atsushi88885@gmail.com>
2019-02-27 09:33:03 +0800

    create testCase for add task

---
+++ tests/Feature/TaskTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Http\Requests\CreateTask;
+use Carbon\Carbon;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class TaskTest extends TestCase
+{
+    // テストケースごとにデータベースをリフレッシュしてマイグレーションを再実行する
+    use RefreshDatabase;
+
+    /**
+     * 期限日が日付ではない場合はバリデーションエラー
+     * @test
+     */
+    // public function due_date_should_be_date()
+    // {
+    //     $response = $this->post('/tasks/create', [
+    //         'title' => 'Sample task',
+    //         'due_date' => 123, // 不正なデータ（数値）
+    //     ]);
+
+    //     $response->assertSessionHasErrors([
+    //         'due_date' => '期限日 には日付を入力してください。',
+    //     ]);
+    // }
+
+    /**
+     * 期限日が過去日付の場合はバリデーションエラー
+     * @test
+     */
+    public function due_date_should_not_be_past()
+    {
+        $response = $this->post('/tasks/create', [
+            'title' => 'Sample task',
+            'due_date' => Carbon::yesterday()->format('Y/m/d'), // 不正なデータ（昨日の日付）
+        ]);
+
+        $response->assertSessionHasErrors([
+            'due_date' => '期限日 には今日以降の日付を入力してください。',
+        ]);
+    }
+}
