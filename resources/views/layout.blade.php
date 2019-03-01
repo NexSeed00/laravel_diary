cbf5ea43529f4dc999e64e466297e1ce0ea0ada9
Atsushi <atsushi88885@gmail.com>
2019-03-01 12:07:23 +0800

    navbarのレイアウト修正

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,24 +6,25 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>ToDo App</title>
   @yield('styles')
+  <link rel="stylesheet" href="/css/app.css">
   <link rel="stylesheet" href="/css/styles.css">
 </head>
 <body>
-<header>
-  <nav class="my-navbar">
-    <a class="my-navbar-brand" href="/">ToDo App</a>
-    <div class="my-navbar-control">
+<header class="mb-5">
+  <nav class="bg-dark nav pl-5 pr-5 pt-2 pb-2 justify-content-between">
+    <a class="navbar-brand text-light" href="/">ToDo App</a>
+    <div class="navbar-brand">
       @if(Auth::check())
-        <span class="my-navbar-item">ようこそ, {{ Auth::user()->name }}さん</span>
+        <span class="text-light">ようこそ, {{ Auth::user()->name }}さん</span>
         ｜
-        <a href="#" id="logout" class="my-navbar-item">ログアウト</a>
+        <a href="#" id="logout" class="text-light">ログアウト</a>
         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
           @csrf
         </form>
       @else
-        <a class="my-navbar-item" href="{{ route('login') }}">ログイン</a>
+        <a class="text-light" href="{{ route('login') }}">ログイン</a>
         ｜
-        <a class="my-navbar-item" href="{{ route('register') }}">会員登録</a>
+        <a class="text-light" href="{{ route('register') }}">会員登録</a>
       @endif
     </div>
   </nav>
