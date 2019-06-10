c480f9865235765d27868d1c634cb9ed76864e35
Atsushi <atsushi88885@gmail.com>
2019-06-11 01:40:02 +0800

    accessor

--- app/Diary.php
+++ app/Diary.php
@@ -3,10 +3,36 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use TheSeer\Tokenizer\Exception;
 
 class Diary extends Model
 {
-    //
+    const STATUS = [
+        'applying',
+        'attended',
+        'denied',
+    ];
+
+    public function getUserIdTextAttribute()
+    {
+        // $this->user_id_text
+
+        // switch($this->attributes['status']) {
+        //     case 1:
+        //         return 'applying';
+        //     case 2:
+        //         return 'attended';
+        //     case 3:
+        //         return 'denied';
+        //     default:
+        //         return '??';
+        // }
+
+        $status = $this->attributes['status'];
+
+        return self::STATUS[$status];
+
+    }
 
     public function likes()
     {
